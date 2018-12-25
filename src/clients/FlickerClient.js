308f696b41872d537d06bf8c9abfb8e077d3b927
Daisuke KONISHI <skydai1151@gmail.com>
2018-12-25 23:55:37 +0900

    Hotfix move query set to flicker provider

--- src/clients/FlickerClient.js
+++ src/clients/FlickerClient.js
@@ -3,11 +3,24 @@ import ErrorService from '../service/ErrorService'
 
 import { config } from '../config'
 
+const { FLICKER_KEY } = process.env
+
 class FlickerClient {
-  static get(component, params) {
+  static get(component, keyword) {
+    const query = {
+      api_key: FLICKER_KEY,
+      format: 'json',
+      nojsoncallback: 1,
+      extras: 'url_l, owner_name',
+      method: 'flickr.photos.search',
+      per_page: 1
+    }
+
+    query.text = keyword
+
     new Server().get(
       `${config.FUNCTION_URL}/flicker`,
-      params,
+      query,
       response => {
         if (response.data.photos.photo.length) {
           component.setState({
